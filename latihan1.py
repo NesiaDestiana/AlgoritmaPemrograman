7348b2d82bcb34878b519b43908630a905a8f4e6
NesiaDestiana <136700108+NesiaDestiana@users.noreply.github.com>
2023-06-16 20:03:37 +0700

    Create latihan1.py
    Latihan1 Algoritma Pemrograman

---
+++ latihan1.py
@@ -0,0 +1,13 @@
+
+nim = input("Masukkan Nim Anda: ")
+nama = input("Maukkan Nama Lengkap: ")
+kelas = input("Masukkan Kelas Anda: ")
+usia = input("Masukkan usia: ")
+print ("================================")
+print ("IDENTITAS MAHASISWA")
+print ("================================")
+print ("NIM Mahasiswa :",nim)
+print ("Nama Lengkap :",nama)
+print ("Kelas Saat Ini:",kelas)
+print ("Usia Anda:",usia)
+print ("================================")
